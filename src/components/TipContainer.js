baf5c5595423a3f21932af72f84f5b9fa32e6979
Blake Johnson <blake41@gmail.com>
2018-08-20 13:52:29 -0400

    handle error cases in metamask where its locked or doesnt exist

--- src/components/TipContainer.js
+++ src/components/TipContainer.js
@@ -6,8 +6,9 @@ class TipContainer extends Component {
 	}
 
   handleClick() {
-    if (typeof Web3 === 'undefined') {
-      return console.log('You need to install MetaMask to use this feature.  https://metamask.io')
+		var user_address = undefined;
+    if (typeof web3 === 'undefined') {
+      return alert('You need to install MetaMask to use this feature.  https://metamask.io')
     }
 		var localWeb3 = new Web3(window.web3.currentProvider)
 		const getAccount = async () => {
@@ -15,8 +16,12 @@ class TipContainer extends Component {
 			return accounts[0]
 		};
 		const sendEth = async () => {
-			var user_address = await getAccount()
-	    localWeb3.eth.sendTransaction({
+			user_address  = await getAccount()
+			if(user_address === undefined) {
+				alert('please unlock metamask by inputting your password')
+				return false
+			}
+			localWeb3.eth.sendTransaction({
 	      to: '0xf17f52151ebef6c7334fad080c5704d77216b732',
 	      from: user_address,
 	      value: localWeb3.utils.toWei('1', 'wei'),
